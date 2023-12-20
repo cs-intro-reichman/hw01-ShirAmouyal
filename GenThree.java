25d1c461bf249a1defb12d3816f294bf3da186d9
Your Name <shir.amoyal@post.runi.ac.il>
2023-12-20 21:57:51 +0200

    with changes

--- GenThree.java
+++ GenThree.java
@@ -2,6 +2,7 @@
  * Generates three random integers, each in a given range [a,b),
  * prints them, and then prints the minimal number that was generated.
  */
+import java.lang.*;
 import java.util.Random;
 public class GenThree {
 	public static void main(String[] args) {
@@ -9,17 +10,19 @@ public class GenThree {
      int a=Integer.parseInt(args[0]);
      int b=Integer.parseInt(args[1]);
      int m=0;
-     int min=0;
+     int min=Math.min(a,b);
+     int max=Math.max(a,b);
+     int currentMin=max;
 
     Random random=new Random();
     while (m<3)
     {
-     int rnd=random.nextInt();
-     if(rnd<min){min=rnd;}
+     int rnd=random.nextInt(max-min)+min;
+     if(rnd<currentMin){currentMin=rnd;}
      System.out.println(rnd);
      m++;
     }
      
-    System.out.println("the minimum number is:"+min);	
+    System.out.println("the minimum number is:"+currentMin);	
 	}
 }
--- LinearEq.java
+++ LinearEq.java
@@ -12,6 +12,6 @@ public class LinearEq {
      double c=Double.parseDouble(args[2]);
      double result=(c-b)/a;
      
-     System.out.println(a+"*x+"+b"="+c+"  x="+result);
+     System.out.println(a+"*x+"+b+"="+c+"  x="+result);
     }
 }
